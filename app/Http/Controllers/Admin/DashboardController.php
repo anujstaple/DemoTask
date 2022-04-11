03f81ed275db2261a1c1068031f5bc92021ab000
anuj <anujkumar@staplelogic.in>
2022-04-11 16:37:47 +0530

    update admin dashbaord

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -46,33 +46,24 @@ class DashboardController extends Controller
            //Count of all active products
             $CountactiveProduct = Product::where('status','active')->count();
 
-            $UserProducts = UserProduct::select('product_id','user_id')->where('status','active')->get();
-
-             $PrIds=[];
-            $userIds=[];
-
-            if($UserProducts){
-                foreach ($UserProducts as $key => $pro) {
-                    $PrIds[]=$pro->product_id;
-                    $userIds[]=$pro->user_id;
-                }
-            }
-
+           
             //Count of active and verified users who have attached active products.
 
-            $CountactiveVerifiedAttachUser =  User::where('status','active')->whereNotNull('email_verified_at')->whereIn('id',[$userIds])->count();
+          
+          $CountactiveVerifiedAttachUser =  User::join('user_product','users.id','=','user_product.user_id')->where('users.status','active')->whereNotNull('users.email_verified_at')->distinct('user_product.user_id')->count();
              
-
+                   
 
            // Count of active products which don't belong to any user.
-
-              if($PrIds){
-                $CountactiveProduct_withoutAttach = Product::where('status','active')->whereNotIn('id',[$PrIds])->count();
-             }
+          
+          $CountactiveProduct_withoutAttach = Product::leftjoin('user_product','products.id', '=', 'user_product.product_id')
+                ->where('products.status','active')
+                ->whereNull('user_product.product_id')
+                ->count();
 
              //Amount of all active attached products 
 
-             $AmountactiveAttachedProduct = UserProduct::select('products.price','products.id as product_id','user_product.qty','products.status')
+             $AmountactiveAttachedProduct = UserProduct::select('user_product.price','products.id as product_id','user_product.qty','products.status')
              ->join('products','products.id','=','user_product.product_id')->where('products.status','active')->sum(DB::raw('user_product.qty'));
 
 
@@ -101,15 +92,15 @@ class DashboardController extends Controller
             
             //Summarized price of all active attached products (from the previous subpoint if prod1 price is 100$, prod2 price is 120$, prod3 price is 200$, the summarized price will be 3 x 100 + 9 x 120 = 1380).
 
-             $PriceActiveAttachedProduct = UserProduct::select('products.price','products.id as product_id','user_product.qty','products.status')
-             ->join('products','products.id','=','user_product.product_id')->where('products.status','active')->sum(DB::raw('products.price * user_product.qty * '.$currencyPrice));
+             $PriceActiveAttachedProduct = UserProduct::select('user_product.price','products.id as product_id','user_product.qty','products.status')
+             ->join('products','products.id','=','user_product.product_id')->where('products.status','active')->sum(DB::raw('user_product.price * user_product.qty * '.$currencyPrice));
 
              $PriceActiveAttachedProduct  = round($PriceActiveAttachedProduct,2);
 
 
             // Summarized prices of all active products per user. For example - John Summer - 85$, Lennon Green - 107$.
 
-              $userSummarizedPrice = UserProduct::select('products.price','products.title','products.id as product_id','user_product.qty','products.status','users.id as user_id','users.name',DB::raw('products.price * user_product.qty * '.$currencyPrice.' as totalAmount'))
+              $userSummarizedPrice = UserProduct::select('user_product.price','products.title','products.id as product_id','user_product.qty','products.status','users.id as user_id','users.name',DB::raw('user_product.price * user_product.qty * '.$currencyPrice.' as totalAmount'))
              ->join('products','products.id','=','user_product.product_id')
               ->join('users','users.id','=','user_product.user_id')
              ->where('products.status','active')->get();
